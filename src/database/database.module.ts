e09482cc282d05c0d936c978e86886c57108a24d
David Restrepo <luisrestrepo1995@gmail.com>
2025-07-28 00:48:45 -0500

    feat: enhance database configuration for SQLite and PostgreSQL support

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -4,19 +4,34 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true, envFilePath: ['src/.env'] }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`src/.env.${process.env.NODE_ENV}.local`],
+    }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        database: config.get('DATABASE_HOST'),
-        port: config.get('DATABASE_PORT'),
-        username: config.get('DATABASE_USER'),
-        password: config.get('DATABASE_PASSWORD'),
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
+      useFactory: (config: ConfigService) => {
+        if (config.get<string>('DATABASE_TYPE') === 'sqlite') {
+          return {
+            type: 'sqlite',
+            database: config.get<string>('DATABASE_NAME'),
+            autoLoadEntities: true,
+            synchronize: true, // Be cautious with this in production
+          };
+        } else {
+          return {
+            type: 'postgres',
+            host: config.get<string>('DATABASE_HOST'),
+            port: config.get<number>('DATABASE_PORT'),
+            username: config.get<string>('DATABASE_USER'),
+            password: config.get<string>('DATABASE_PASSWORD'),
+            database: config.get<string>('DATABASE_NAME'),
+            autoLoadEntities: true,
+            synchronize: false, // Strongly recommended to be false in production
+          };
+        }
+      },
     }),
   ],
 })
